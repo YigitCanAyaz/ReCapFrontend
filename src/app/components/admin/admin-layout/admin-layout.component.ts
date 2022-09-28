5c3d568a8c13b774bdfd2e68ab366c1e3941014e
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-09-28 22:59:26 +0300

    changed site and admin layout

--- src/app/components/admin/admin-layout/admin-layout.component.ts
+++ src/app/components/admin/admin-layout/admin-layout.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminLayoutComponent implements OnInit {
 
-  constructor() { }
+  brandActive: boolean = false;
+  carActive: boolean = false;
+  customerActive: boolean = false;
+  colorActive: boolean = false;
+  modelActive: boolean = false;
+  rentalActive: boolean = false;
+
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    let currentComponent = this.router.url.split('/')[2];
+    console.log(currentComponent);
+    if (currentComponent !== undefined) {
+      this.setAndRemoveActive(currentComponent);
+    }
+  }
+
+  checkActive(menuActive: boolean): string {
+    return menuActive ? "active" : "";
+  }
+
+  setAndRemoveActive(menuName: string) {
+
+    this.brandActive = false;
+    this.carActive = false;
+    this.customerActive = false;
+    this.colorActive = false;
+    this.modelActive = false;
+    this.rentalActive = false;
+
+
+    switch (menuName) {
+      case 'brands':
+        this.brandActive = true;
+        break;
+      case 'cars':
+        this.carActive = true;
+        break;
+      case 'colors':
+        this.colorActive = true;
+        break;
+      case 'customers':
+        this.customerActive = true;
+        break;
+      case 'models':
+        this.modelActive = true;
+        break;
+      case 'rentals':
+        this.rentalActive = true;
+        break;
+
+      default:
+        break;
+    }
   }
 
 }

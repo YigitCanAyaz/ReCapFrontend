6c3df1c7a73db5bd1cee13b813e8b692dca48b14
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-09-28 15:42:48 +0300

    filled routing module with crud admin operations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,30 @@ import { ColorListComponent } from './components/admin/color/color-list/color-li
 import { CustomerListComponent } from './components/admin/customer/customer-list/customer-list.component';
 import { ModelListComponent } from './components/admin/model/model-list/model-list.component';
 import { RentalListComponent } from './components/admin/rental/rental-list/rental-list.component';
+import { BrandViewComponent } from './components/admin/brand/brand-view/brand-view.component';
+import { CarViewComponent } from './components/admin/car/car-view/car-view.component';
+import { ColorViewComponent } from './components/admin/color/color-view/color-view.component';
+import { CustomerViewComponent } from './components/admin/customer/customer-view/customer-view.component';
+import { ModelViewComponent } from './components/admin/model/model-view/model-view.component';
+import { RentalViewComponent } from './components/admin/rental/rental-view/rental-view.component';
+import { BrandAddComponent } from './components/admin/brand/brand-add/brand-add.component';
+import { CarAddComponent } from './components/admin/car/car-add/car-add.component';
+import { ColorAddComponent } from './components/admin/color/color-add/color-add.component';
+import { CustomerAddComponent } from './components/admin/customer/customer-add/customer-add.component';
+import { ModelAddComponent } from './components/admin/model/model-add/model-add.component';
+import { RentalAddComponent } from './components/admin/rental/rental-add/rental-add.component';
+import { BrandUpdateComponent } from './components/admin/brand/brand-update/brand-update.component';
+import { CarUpdateComponent } from './components/admin/car/car-update/car-update.component';
+import { ColorUpdateComponent } from './components/admin/color/color-update/color-update.component';
+import { CustomerUpdateComponent } from './components/admin/customer/customer-update/customer-update.component';
+import { ModelUpdateComponent } from './components/admin/model/model-update/model-update.component';
+import { RentalUpdateComponent } from './components/admin/rental/rental-update/rental-update.component';
+import { BrandRemoveComponent } from './components/admin/brand/brand-remove/brand-remove.component';
+import { CarRemoveComponent } from './components/admin/car/car-remove/car-remove.component';
+import { ColorRemoveComponent } from './components/admin/color/color-remove/color-remove.component';
+import { CustomerRemoveComponent } from './components/admin/customer/customer-remove/customer-remove.component';
+import { ModelRemoveComponent } from './components/admin/model/model-remove/model-remove.component';
+import { RentalRemoveComponent } from './components/admin/rental/rental-remove/rental-remove.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -40,6 +64,35 @@ const routes: Routes = [
       { path: "customers/list", component: CustomerListComponent },
       { path: "models/list", component: ModelListComponent },
       { path: "rentals/list", component: RentalListComponent },
+
+      { path: "brands/view/:brandId", component: BrandViewComponent },
+      { path: "cars/view/:carId", component: CarViewComponent },
+      { path: "colors/view/:colorId", component: ColorViewComponent },
+      { path: "customers/view/:customerId", component: CustomerViewComponent },
+      { path: "models/view/:modelId", component: ModelViewComponent },
+      { path: "rentals/view/:rentalId", component: RentalViewComponent },
+
+      { path: "brands/add/:brandId", component: BrandAddComponent },
+      { path: "cars/add/:carId", component: CarAddComponent },
+      { path: "colors/add/:colorId", component: ColorAddComponent },
+      { path: "customers/add/:customerId", component: CustomerAddComponent },
+      { path: "models/add/:modelId", component: ModelAddComponent },
+      { path: "rentals/add/:rentalId", component: RentalAddComponent },
+
+      { path: "brands/update/:brandId", component: BrandUpdateComponent },
+      { path: "cars/update/:carId", component: CarUpdateComponent },
+      { path: "colors/update/:colorId", component: ColorUpdateComponent },
+      { path: "customers/update/:customerId", component: CustomerUpdateComponent },
+      { path: "models/update/:modelId", component: ModelUpdateComponent },
+      { path: "rentals/update/:rentalId", component: RentalUpdateComponent },
+
+      { path: "brands/remove/:brandId", component: BrandRemoveComponent },
+      { path: "cars/remove/:carId", component: CarRemoveComponent },
+      { path: "colors/remove/:colorId", component: ColorRemoveComponent },
+      { path: "customers/remove/:customerId", component: CustomerRemoveComponent },
+      { path: "models/remove/:modelId", component: ModelRemoveComponent },
+      { path: "rentals/remove/:rentalId", component: RentalRemoveComponent },
+
     ]
   },
 ];

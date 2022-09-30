ccdb1e3bd0270734a7a7fc890eade266b5411cec
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-09-30 17:03:48 +0300

    created userOperation and operations's concrete service

---
+++ src/app/services/abstract/iOperationClaimService.ts
@@ -0,0 +1,11 @@
+import { Observable } from "rxjs";
+import { OperationClaim } from "src/app/models/entities/operationClaim";
+import { ListResponseModel } from "src/app/models/listResponseModel";
+import { ResponseModel } from "src/app/models/responseModel";
+import { SingleResponseModel } from "src/app/models/singleResponseModel";
+
+export interface IOperationClaimService {
+  getAll(): Observable<ListResponseModel<OperationClaim>>;
+  getById(id: number): Observable<SingleResponseModel<OperationClaim>>;
+  add(operationClaim: OperationClaim): Observable<ResponseModel>;
+}

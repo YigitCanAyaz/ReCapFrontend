4cb3bf01c86e58de010c24bc298132fe59a9a39d
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-10-01 23:31:49 +0300

    filled user crud operations

--- src/app/services/abstract/iCustomerService.ts
+++ src/app/services/abstract/iCustomerService.ts
@@ -3,10 +3,12 @@ import { Observable } from "rxjs";
 import { CustomerDetail } from "src/app/models/details/customerDetail";
 import { Customer } from "src/app/models/entities/customer";
 import { ListResponseModel } from "src/app/models/listResponseModel";
+import { ResponseModel } from 'src/app/models/responseModel';
 
 export interface ICustomerService {
   getAll(): Observable<ListResponseModel<Customer>>;
   getById(id: number): Observable<SingleResponseModel<Customer>>;
   getAllCustomerDetails(): Observable<ListResponseModel<CustomerDetail>>;
   getCustomerDetailsById(id: number): Observable<SingleResponseModel<CustomerDetail>>;
+  add(customer: Customer): Observable<ResponseModel>;
 }

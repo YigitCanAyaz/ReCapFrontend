0f40fae5d4bc8f7ef16db0b0bd1c882e6c0e26f3
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-10-03 16:44:21 +0300

    added add operation to rental service

--- src/app/services/abstract/iRentalService.ts
+++ src/app/services/abstract/iRentalService.ts
@@ -2,6 +2,7 @@ import { Observable } from "rxjs";
 import { RentalDetail } from "src/app/models/details/rentalDetail";
 import { Rental } from "src/app/models/entities/rental";
 import { ListResponseModel } from "src/app/models/listResponseModel";
+import { ResponseModel } from "src/app/models/responseModel";
 import { SingleResponseModel } from "src/app/models/singleResponseModel";
 
 export interface IRentalService {
@@ -9,4 +10,6 @@ export interface IRentalService {
   getById(id: number): Observable<SingleResponseModel<Rental>>;
   getAllRentalDetails(): Observable<ListResponseModel<RentalDetail>>;
   getRentalDetailsById(id: number): Observable<SingleResponseModel<RentalDetail>>;
+
+  add(rental: Rental): Observable<ResponseModel>;
 }
--- src/app/services/concrete/rental.service.ts
+++ src/app/services/concrete/rental.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { RentalDetail } from 'src/app/models/details/rentalDetail';
 import { Rental } from 'src/app/models/entities/rental';
 import { ListResponseModel } from 'src/app/models/listResponseModel';
+import { ResponseModel } from 'src/app/models/responseModel';
 import { SingleResponseModel } from 'src/app/models/singleResponseModel';
 import { IRentalService } from '../abstract/iRentalService';
 
@@ -45,6 +46,10 @@ export class RentalService implements IRentalService {
   getAllRentalDetailsByCustomerId(customerId: number): Observable<ListResponseModel<RentalDetail>> {
     const path = this.baseUrl + "getallrentaldetailsbycustomerid?customerid=" + customerId;
     return this.httpClient.get<ListResponseModel<RentalDetail>>(path);
+  }
+
+  add(rental: Rental): Observable<ResponseModel> {
+    return this.httpClient.post<ResponseModel>(this.baseUrl + "add", rental);
   }
 
 }

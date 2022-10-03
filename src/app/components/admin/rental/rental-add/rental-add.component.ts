b2c1e4043e0e586f582b8b865739e0acaa67a969
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-10-03 16:43:55 +0300

    added add operation to rental service

--- src/app/components/admin/rental/rental-add/rental-add.component.ts
+++ src/app/components/admin/rental/rental-add/rental-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RentalService } from 'src/app/services/concrete/rental.service';
 
 @Component({
   selector: 'app-rental-add',
@@ -7,9 +8,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RentalAddComponent implements OnInit {
 
-  constructor() { }
+  rentalAddForm: FormGroup;
+  models: Model[] = [];
+  colors: Color[] = [];
+
+  constructor(private rentalService: RentalService, private modelService: ModelService, private colorService: ColorService, private formBuilder: FormBuilder, private toastrService: ToastrService, private router: Router) { }
 
   ngOnInit(): void {
+    this.createRentalAddForm();
+    this.getAllModels();
+    this.getAllColors();
   }
 
+  createRentalAddForm(): void {
+    this.rentalAddForm = this.formBuilder.group({
+      modelId: ["", [Validators.required]],
+      colorId: ["", [Validators.required]]
+    });
+  }
+
+  addRental(): void {
+    console.log(this.rentalAddForm.value);
+    if (this.rentalAddForm.valid) {
+      this.rentalService.add(this.rentalAddForm.value).subscribe(response => {
+        this.toastrService.info(response.message);
+        this.router.navigate(["/admin/rentals/list"]);
+      }, responseError => {
+        this.toastrService.error(responseError.error);
+      });
+    }
+
+    else {
+      this.toastrService.error("Fill the form correctly");
+    }
+  }
+
+  getAllModels(): void {
+    this.modelService.getAll().subscribe(response => {
+      this.models = response.data;
+    })
+  }
+
+  getAllColors(): void {
+    this.colorService.getAll().subscribe(response => {
+      this.colors = response.data;
+    })
+  }
 }

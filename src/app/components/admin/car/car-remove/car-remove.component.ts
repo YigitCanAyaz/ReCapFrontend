f87dd9578026e69a1473763b01ee719289658759
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2022-10-04 23:11:17 +0300

    added delete operation for abstract services

--- src/app/components/admin/car/car-remove/car-remove.component.ts
+++ src/app/components/admin/car/car-remove/car-remove.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Car } from 'src/app/models/entities/car';
+import { CarService } from 'src/app/services/concrete/car.service';
 
 @Component({
   selector: 'app-car-remove',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarRemoveComponent implements OnInit {
 
-  constructor() { }
+  constructor(private carService: CarService, private activatedRoute: ActivatedRoute, private toastrService: ToastrService, private router: Router) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      if (params["carId"]) {
+        this.getCarById(params["carId"]);
+      }
+    });
+  }
+
+  getCarById(id: number): void {
+    this.carService.getById(id).subscribe(response => {
+      this.removeCar(response.data);
+    })
+  }
+
+  removeCar(car: Car): void {
+    this.carService.delete(car).subscribe(response => {
+      this.toastrService.warning(response.message);
+      this.router.navigate(['/admin/cars/list']);
+    });
   }
 
 }
